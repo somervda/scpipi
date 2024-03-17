5988fbb3e2feae0872540888e7c1309f23b3bb5f
scpipi <davidsomerville@comcast.net>
2024-03-17 09:44:41 -0400

    WIP

--- sketchpad.py
+++ sketchpad.py
@@ -1,5 +1,9 @@
 import pyvisa
+import time
 rm = pyvisa.ResourceManager('@py')
 print(rm.list_resources())
 owon1241 = rm.open_resource('ASRL/dev/ttyUSB0::INSTR',baud_rate=115200)
 print(owon1241.query('*IDN?'))
+for x in range(10):
+    print(owon1241.query('MEAS1:SHOW?'))
+    time.sleep(1)
