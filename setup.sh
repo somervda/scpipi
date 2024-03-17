960845a4d91c0f572c60fe012974aff22c6e4054
scpipi <davidsomerville@comcast.net>
2024-03-16 21:07:45 -0400

    setup

---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Before running this file do the following on the raspbery pi
+# Add git and your git info
+# sudo apt -y install git
+# git config --global user.name "scpipi"
+# git config --global user.email ""
+# git clone https://github.com/somervda/scpipi.git
+# This file will now be in the pimidi folder
+
+
+# Make sure apt is updated and we have the latest package lists before we start
+# Remember to 'chmod u+x pimidi_setup.sh' to be able to run this script 
+# then 'bash setup.sh'
+
+date
+echo 1. Updating and Upgrade apt packages 
+sudo apt update -y
+sudo apt upgrade -y
+
+echo 2. Installing and rationalizing Python Version Names
+sudo apt install -y python-is-python3
+sudo apt install -y python3-pip
+sudo apt install -y python-dev-is-python3
+
+python --version
+pip --version
+
+
+echo 3. Installing OPi.GPIO 
+# Install GPIO support for the orange PI 
+# see https://pypi.org/project/RPi.GPIO/ and https://sourceforge.net/p/raspberry-gpio-python/wiki/Home/ 
+# Note: Use GPIO.setmode(GPIO.SUNXI) to use "PA01" style channel naming
+pip install RPi.GPIO
+
+
+echo 4. Installing python i2c and oled support
+sudo usermod -a -G gpio pi
+
+echo 5. Install fastapi for web services and a ASGI web server
+pip install fastapi
+pip install "uvicorn[standard]"
+# Note: I run uvicorn using this command during development
+# uvicorn web:app --reload --host pimidi.home
+
+
+# Add uvicorn.service to the /lib/systemd/system/ folder
+echo 6. Setup the pimidi_uvicorn.service to run on startup 
+sudo cp uvicorn.service /lib/systemd/system/uvicorn.service
+sudo systemctl enable uvicorn.service
+sudo systemctl start uvicorn.service 
+
+date
---
+++ web.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+# The main entrypoint for the scpipi application
+# Runs in a fastAPI server to accept web service calls
+# Note for testing 
+# uvicorn web:app --reload --host scpipi.local
+
+import sys
+import time
+import asyncio
+
+
+# fastAPI 
+from typing import Union,Annotated
+from fastapi import FastAPI,Path
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+# Note: Make sure this line is at the end of the file so fastAPI falls through the other
+# routes before serving up static files 
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
