1138f82008b054e6f8290907558fdbfb87cdba27
scpipi <davidsomerville@comcast.net>
2024-03-20 14:02:02 -0400

    WIP

--- web.py
+++ web.py
@@ -35,6 +35,8 @@ print(rm.list_resources())
 
 
 # owon xpm1241 services
+#  See https://files.owon.com.cn/software/Application/XDM1000_Digital_Multimeter_Programming_Manual.pdf 
+# for scpi commands
 xpm1241 = None
 
 @app.get("/xpm1241/connect")
@@ -56,34 +58,111 @@ def xpm1241Connect():
     else:
         return(False)
 
-@app.get("/xpm1241/config/{type}/{rate}/{range}")
-# type: voltdc,voltac,currdc,currac,res,temp
-# rate: S/M/F (Slow/Medium/Fast sampling)
-# range: auto,1-9
+@app.get("/xpm1241/config/{type}/{range}/{rate}")
 async def xpm1241Config(type: Annotated[str, Path(title="type: voltdc,voltac,currdc,currac,res,temp")],
-    rate: Annotated[str, Path(title="rate: S/M/F (Slow/Medium/Fast sampling)")],
-    range: Annotated[str, Path(title="range: auto,1-9")]): 
+    range: Annotated[int, Path(title="range: auto=0  or 1-9 for specific range (see programming manual)")], 
+    rate: Annotated[int, Path(title="rate: (0=Slow, 1=Medium 2=Fast)")]):
     global xpm1241
     if not xpm1241:
         # print("Connect to xpm1241")
         xpm1241Connect()
     if xpm1241:
-        # Build configuration string
-        config = "CONFigure:"
+        # Build configCmduration string
+        configCmd = "CONFigure:"
         if type== "voltdc" :
-            config += "VOLT:DC"
+            configCmd += "VOLT:DC"
+            if range == 1:
+                configCmd += " 500E-3"
+            elif range == 2:
+                configCmd += " 5"
+            elif range == 3:
+                configCmd += " 50"
+            elif range == 4:
+                configCmd += " 500"
+            elif range == 5:
+                configCmd += " 1000"
+            else:
+                configCmd += " AUTO"
         elif type ==  "voltac" :
-            config += "VOLT:AC"
+            configCmd += "VOLT:AC"
+            if range == 1:
+                configCmd += " 500E-3"
+            elif range == 2:
+                configCmd += " 5"
+            elif range == 3:
+                configCmd += " 50"
+            elif range == 4:
+                configCmd += " 500"
+            elif range == 5:
+                configCmd += " 750"
+            else:
+                configCmd += ":AUTO"
         elif type ==  "currdc" :
-            config += "CURR:DC"
+            configCmd += "CURR:DC"
+            if range == 1:
+                configCmd += " 5E-3"
+            elif range == 2:
+                configCmd += " 50E-3"
+            elif range == 3:
+                configCmd += " 500E-3"
+            elif range == 4:
+                configCmd += " 5"
+            elif range == 4:
+                configCmd += " 10"
+            else:
+                configCmd += " AUTO"
         elif type ==   "currac" :
-            config += "CURR:AC"
+            configCmd += "CURR:AC"
+            if range == 1:
+                configCmd += " 5E-3"
+            elif range == 2:
+                configCmd += " 50E-3"
+            elif range == 3:
+                configCmd += " 500E-3"
+            elif range == 4:
+                configCmd += " 5"
+            elif range == 4:
+                configCmd += " 10"
+            else:
+                configCmd += " AUTO"
         elif type ==   "res" :
-            config += "RES"
+            configCmd += "RES"
+            if range == 1:
+                configCmd += " 500"
+            elif range == 2:
+                configCmd += " 5E3"
+            elif range == 3:
+                configCmd += " 50E3"
+            elif range == 4:
+                configCmd += " 500E3"
+            elif range == 5:
+                configCmd += " 5E6"
+            elif range == 6:
+                configCmd += " 50E6"
+            elif range == 7:
+                configCmd += " 500E6"
+            else:
+                configCmd += " AUTO"
+        elif type ==  "freq" :
+            configCmd += "FREQ"
         elif type ==  "temp" :
-            config += "TEMP"
+            configCmd += "TEMP"
+        print(configCmd)
+        # Also build the rate command
+        rateCmd = "RATE "
+        if rate== 1 :
+            rateCmd += "M"
+        elif rate == 2 :
+            rateCmd += "F"
+        else:
+            rateCmd += "S"
+
         try:
-            result= xpm1241.write(config)
+            # Set rate,type and range
+            result= xpm1241.write(rateCmd)
+            time.sleep(.1)
+            result= xpm1241.write(configCmd)
+            time.sleep(.1)
             return{True}
         except OSError:
             # print("xpm1241 oserror")
@@ -95,7 +174,7 @@ async def xpm1241Config(type: Annotated[str, Path(title="type: voltdc,voltac,cur
 
 
 @app.get("/xpm1241/measure")
-async def xpm1241Config(): 
+async def xpm1241configCmd(): 
     global xpm1241
     if not xpm1241:
         # print("Connect to xpm1241")
