fbb13edfbc3e84b760071df3805c12c56cc6f270
scpipi <davidsomerville@comcast.net>
2024-04-07 08:37:48 -0400

    Updated scpipi_ui

--- scripts/autoSketch.py
+++ scripts/autoSketch.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import sys
 sys.path.append("lib")
 
@@ -16,40 +17,24 @@ result=[]
 start=time.time()
 step=1
 tableJson=""
-jds6600 = Jds6600(quiet=True)
-if not jds6600.connect():
-    print("Error: jds6600 signal generator did not connect, ending....")
-    exit(0)
-freq=8
-jds6600.configure(freq,5,0)
-time.sleep(3)
 sds1052 = Sds1052(quiet=True)
 if not sds1052.connect():
     print("Error: Siglent sds1052 oscilloscope did not connect, ending....")
     exit(0)
-time.sleep(3)
+
 #  Main automation loop
-while (start + 2000) > time.time():
-    helper.writeStatus("auto","running",step,"")
+while (start + 20) > time.time():
     rowJson=helper.startRow(step)
-    rowJson=helper.addRowMeasurement(rowJson,"frequency","",freq)
+    helper.writeStatus("auto","running",step,"")
 
     #  Collect Measurements
     measure=sds1052.measure("PKPK")
     rowJson=helper.addRowMeasurement(rowJson,"sds1052","PKPK",str(measure["measure"]))
-    measure=sds1052.measure("RMS")
-    rowJson=helper.addRowMeasurement(rowJson,"sds1052","RMS",str(measure["measure"]))
     print("rowJson:",rowJson)
     tableJson=helper.addTableRow(tableJson,rowJson)
-
-    #  Update JDS6600 frequency
-    freq *= 1.059
-    freq = round(freq, 1)
-    if freq>20000:
-        break
-    jds6600.configure(freq,5,0)
     time.sleep(1)
     step+=1
 
 #  Write the results
 helper.writeJsonTable("auto",tableJson)
+helper.removeStatus()
